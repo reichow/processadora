fb3f83cb80afa3df1fd9305753af9c7ecaa4a2e3
Camila <camila.reichow@cwi.com.br>
2019-04-12 15:37:06 -0300

    commit inicial

---
+++ src/main/java/br/com/camila/processadora/ProcessadoraApplication.java
@@ -0,0 +1,13 @@
+package br.com.camila.processadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessadoraApplication.class, args);
+	}
+
+}
